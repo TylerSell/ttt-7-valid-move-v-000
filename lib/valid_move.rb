0b8164964e2f67888768408c6eedeccba0738d31
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-07-26 18:19:38 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def valid_move?(board, index)
   def between(index)
     if index >= 0 && index <= 8
@@ -11,11 +12,30 @@ def valid_move?(board, index)
   elsif position_taken?(board, index) == true
     false
   elsif between(index) == false
+=======
+def between(index)
+  if index >= 0 && index <= 8
+    true
+  else 
+>>>>>>> e5f1246bde2f3d88419db5e685d3ea40a21aa68e
     false
   end
 end
 
+def valid_move?(board, index)
+  if position_taken == false && between == true
+    true
+  elsif position_taken == true
+    false
+  elsif between == false
+    false
+  end
+end
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> e5f1246bde2f3d88419db5e685d3ea40a21aa68e
 def position_taken?(board, index)
   if board[index] == " "
     false
